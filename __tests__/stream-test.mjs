dbb2213d8c8fd6d5fe68f3281be201adebd05e5c
Keita Urashima <ursm@ursm.jp>
2019-04-08 07:33:51 +0900

    pull up

--- __tests__/stream-test.mjs
+++ __tests__/stream-test.mjs
@@ -6,116 +6,114 @@ import { createFlecktarnUrl } from '../util'
 jest.mock('node-fetch')
 jest.mock('twitter-lite')
 
-describe('stream', () => {
-  const config = {
-    idobata: {
-      hookEndpoint: 'https://example.com/idobata-hook'
-    },
-    twitter: {
-      follow: 'FOLLOW_USER_ID'
-    },
-    flecktarn: {
-      url:        'https://example.com/flecktarn',
-      hmacSecret: 'SECRET'
-    }
+const config = {
+  idobata: {
+    hookEndpoint: 'https://example.com/idobata-hook'
+  },
+  twitter: {
+    follow: 'FOLLOW_USER_ID'
+  },
+  flecktarn: {
+    url:        'https://example.com/flecktarn',
+    hmacSecret: 'SECRET'
   }
+}
 
-  let stream
+let stream
 
-  beforeEach(() => {
-    fetch.mockReset()
+beforeEach(() => {
+  fetch.mockReset()
 
-    stream = createStream(config)
+  stream = createStream(config)
+})
+
+describe('on tweet', () => {
+  test('anyone else', () => {
+    stream.emit('data', {
+      user: {
+        id_str: 'SOMEBODY_ID'
+      }
+    })
+
+    expect(fetch).not.toHaveBeenCalled()
   })
 
-  describe('on tweet', () => {
-    test('anyone else', () => {
-      stream.emit('data', {
-        user: {
-          id_str: 'SOMEBODY_ID'
-        }
-      })
-
-      expect(fetch).not.toHaveBeenCalled()
+  test('simple', () => {
+    stream.emit('data', {
+      user: {
+        id_str: 'FOLLOW_USER_ID',
+        profile_image_url_https: 'https://example.com/profile.png',
+        name: 'Alice Liddell',
+        screen_name: 'alice',
+      },
+      id_str: 'TWEET_ID',
+      text: 'hello from the wonderland'
     })
 
-    test('simple', () => {
-      stream.emit('data', {
-        user: {
-          id_str: 'FOLLOW_USER_ID',
-          profile_image_url_https: 'https://example.com/profile.png',
-          name: 'Alice Liddell',
-          screen_name: 'alice',
-        },
-        id_str: 'TWEET_ID',
-        text: 'hello from the wonderland'
-      })
+    expect(fetch).toHaveBeenCalledTimes(1)
 
-      expect(fetch).toHaveBeenCalledTimes(1)
+    const [url, {method, headers, body}] = fetch.mock.calls[0]
+    const {source, format} = JSON.parse(body)
 
-      const [url, {method, headers, body}] = fetch.mock.calls[0]
-      const {source, format} = JSON.parse(body)
+    expect(url).toBe('https://example.com/idobata-hook')
+    expect(method).toBe('post')
+    expect(headers).toEqual({'Content-Type': 'application/json'})
+    expect(format).toBe('html')
 
-      expect(url).toBe('https://example.com/idobata-hook')
-      expect(method).toBe('post')
-      expect(headers).toEqual({'Content-Type': 'application/json'})
-      expect(format).toBe('html')
+    expect(source).toEqualWithUnindent(`
+      <img height="16" width="16" src="https://example.com/profile.png">
+      <b>Alice Liddell</b> (<a href="https://twitter.com/alice">@alice</a>)<br>
+      <p>hello from the wonderland (<a href="https://twitter.com/alice/status/TWEET_ID">link</a>)</p>
+    `)
+  })
 
-      expect(source).toEqualWithUnindent(`
-        <img height="16" width="16" src="https://example.com/profile.png">
-        <b>Alice Liddell</b> (<a href="https://twitter.com/alice">@alice</a>)<br>
-        <p>hello from the wonderland (<a href="https://twitter.com/alice/status/TWEET_ID">link</a>)</p>
-      `)
+  test('extended', () => {
+    const photoUrl     = 'https://example.com/tweet-photo.png'
+    const flecktarnUrl = createFlecktarnUrl(photoUrl, config.flecktarn)
+
+    stream.emit('data', {
+      user: {
+        id_str: 'FOLLOW_USER_ID',
+        profile_image_url_https: 'https://example.com/profile.png',
+        name: 'Alice Liddell',
+        screen_name: 'alice',
+      },
+      id_str: 'TWEET_ID',
+      text: 'hello from the wonderland',
+      extended_tweet: {
+        full_text: 'Alice taking "Drink Me" bottle',
+        display_text_range: [14, 22],
+      },
+      extended_entities: {
+        media: [
+          {
+            type: 'photo',
+            media_url_https: photoUrl
+          },
+          {
+            type: 'other'
+          }
+        ]
+      }
     })
 
-    test('extended', () => {
-      const photoUrl     = 'https://example.com/tweet-photo.png'
-      const flecktarnUrl = createFlecktarnUrl(photoUrl, config.flecktarn)
+    expect(fetch).toHaveBeenCalledTimes(1)
 
-      stream.emit('data', {
-        user: {
-          id_str: 'FOLLOW_USER_ID',
-          profile_image_url_https: 'https://example.com/profile.png',
-          name: 'Alice Liddell',
-          screen_name: 'alice',
-        },
-        id_str: 'TWEET_ID',
-        text: 'hello from the wonderland',
-        extended_tweet: {
-          full_text: 'Alice taking "Drink Me" bottle',
-          display_text_range: [14, 22],
-        },
-        extended_entities: {
-          media: [
-            {
-              type: 'photo',
-              media_url_https: photoUrl
-            },
-            {
-              type: 'other'
-            }
-          ]
-        }
-      })
+    const [, {body}] = fetch.mock.calls[0]
+    const {source} = JSON.parse(body)
 
-      expect(fetch).toHaveBeenCalledTimes(1)
+    expect(source).toEqualWithUnindent(`
+      <img height="16" width="16" src="https://example.com/profile.png">
+      <b>Alice Liddell</b> (<a href="https://twitter.com/alice">@alice</a>)<br>
+      <p>Drink Me (<a href="https://twitter.com/alice/status/TWEET_ID">link</a>)</p>
 
-      const [, {body}] = fetch.mock.calls[0]
-      const {source} = JSON.parse(body)
-
-      expect(source).toEqualWithUnindent(`
-        <img height="16" width="16" src="https://example.com/profile.png">
-        <b>Alice Liddell</b> (<a href="https://twitter.com/alice">@alice</a>)<br>
-        <p>Drink Me (<a href="https://twitter.com/alice/status/TWEET_ID">link</a>)</p>
-
-        <ul class="list-inline">
-          <li>
-            <a href="${flecktarnUrl}">
-              <img src="${flecktarnUrl}" alt="">
-            </a>
-          </li>
-        </ul>
-      `)
-    })
+      <ul class="list-inline">
+        <li>
+          <a href="${flecktarnUrl}">
+            <img src="${flecktarnUrl}" alt="">
+          </a>
+        </li>
+      </ul>
+    `)
   })
 })
